578353d984ceca1df92702e65a7fede1faadd50e
Kent Wong <kent.wong@fdmgroup.com>
2021-08-29 20:53:01 +1000

    feat: post new pet

--- springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
+++ springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class PetController {
 	@GetMapping("/pets")
 	public List<Pet> getAllPets(){
 		return petRepository.findAll();
+	}
+	
+	@PostMapping("/pets")
+	public Pet createPet(@RequestBody Pet pet) {
+		return petRepository.save(pet);
 	}
 }
