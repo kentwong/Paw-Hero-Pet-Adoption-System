a2c699c0c9d0a6728de5d1a9a7e3bc950fd0d8c9
Kent Wong <kent.wong@fdmgroup.com>
2021-08-30 11:23:59 +1000

    feat: add getPetById

--- springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
+++ springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
@@ -1,15 +1,19 @@
 package com.fdmgroup.pawhero.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fdmgroup.pawhero.exception.ResourceNotFoundException;
 import com.fdmgroup.pawhero.model.Pet;
 import com.fdmgroup.pawhero.repositories.PetRepository;
 
@@ -19,15 +23,25 @@ import com.fdmgroup.pawhero.repositories.PetRepository;
 public class PetController {
 
 	@Autowired
-	private PetRepository petRepository;
+	private PetRepository petRepo;
 	
 	@GetMapping("/pets")
 	public List<Pet> getAllPets(){
-		return petRepository.findAll();
+		return petRepo.findAll();
 	}
 	
 	@PostMapping("/pets")
 	public Pet createPet(@RequestBody Pet pet) {
-		return petRepository.save(pet);
+		return petRepo.save(pet);
 	}
+	
+	@GetMapping("/pets/{id}")
+	public ResponseEntity<Pet> getPetById(@PathVariable Integer id) {
+		Pet pet = petRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Pet does not exist with id : " + id));
+		
+		return ResponseEntity.ok(pet);
+	}
+	
+	
 }
