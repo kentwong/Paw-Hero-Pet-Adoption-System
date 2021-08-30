212de309e413eb2a7d86786926df3b55f4fd6b55
Kent Wong <kent.wong@fdmgroup.com>
2021-08-30 15:58:56 +1000

    feat: add deletemapping to rest api

--- springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
+++ springboot-backend/src/main/java/com/fdmgroup/pawhero/controllers/PetController.java
@@ -1,11 +1,14 @@
 package com.fdmgroup.pawhero.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +64,18 @@ public class PetController {
 		
 		Pet updatedPet = petRepo.save(pet);
 		return ResponseEntity.ok(updatedPet);
+	}
+	
+	@DeleteMapping("/pets/{id}")
+	public ResponseEntity<Map<String, Boolean>> deletePet(Integer id) {
+		Pet pet = petRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Pet does not exist with id : " + id));
+		
+		petRepo.delete(pet);
+	
+		Map<String, Boolean> res = new HashMap<>();
+		res.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(res);
 	}
 	
 	
