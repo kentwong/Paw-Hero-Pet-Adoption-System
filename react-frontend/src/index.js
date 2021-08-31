fa7b7fe1cc97790155c4deb697eee65f2e013352
Kent Wong <kent.wong@fdmgroup.com>
2021-08-31 21:58:38 +1000

    feat: add auth reducer

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+export * from './services/auth/authActions'
 
 ReactDOM.render(
   <React.StrictMode>
